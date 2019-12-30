e30cca09e70d9b7d3969a1ea74b2a7ffdb8e2b01
Al <nguyenvtalexander@gmail.com>
2019-12-30 13:08:29 -0800

    added changes comments

--- db/migrate/20191230210252_create_comments.rb
+++ db/migrate/20191230210252_create_comments.rb
@@ -1,8 +1,11 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      
+      t.string :body
+      t.integer :user_id, null: false
+      t.integer :video_id, null: false
       t.timestamps
     end
+    add_index :comments, [:user_id, :video_id]
   end
 end
