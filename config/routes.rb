9d528606a7bbe677467c684c810a2e114f136f85
Al <nguyenvtalexander@gmail.com>
2019-12-30 14:06:35 -0800

    comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   
+  get 'comments/index'
+  get 'comments/show'
+  get 'comments/create'
+  get 'comments/destroy'
+  get 'comments/update'
   get 'root/root'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
