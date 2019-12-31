a526f5e083f44fe5cd13c58bc1a648df1099982c
Al <nguyenvtalexander@gmail.com>
2019-12-30 17:38:41 -0800

    all comments action

--- frontend/actions/comment_actions.js
+++ frontend/actions/comment_actions.js
@@ -4,4 +4,9 @@ export const RECEIVE_ALL_COMMENTS = 'RECEIVE_ALL_COMMENTS';
 export const RECEIVE_COMMENT = 'RECEIVE_COMMENT';
 export const REMOVE_COMMENT = 'REMOVE_COMMENT';
 
-
+const receiveAllComments = comments => {
+    return {
+        type: RECEIVE_ALL_COMMENTS,
+        comments
+    };
+};
