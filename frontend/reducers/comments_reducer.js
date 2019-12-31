a4c6af3d36bc0cda2dbab6fd7723989141613459
Al <nguyenvtalexander@gmail.com>
2019-12-30 20:39:22 -0800

    comments reducer

---
+++ frontend/reducers/comments_reducer.js
@@ -0,0 +1,23 @@
+import {
+    RECEIVE_ALL_COMMENTS,
+    RECEIVE_COMMENT,
+    REMOVE_COMMENT
+} from '../actions/comment_actions';
+
+const commentsReducer = (oldState = {}, action) => {
+    Object.freeze(oldState);
+    switch(action.type) {
+        case RECEIVE_ALL_COMMENTS:
+            return Object.assign({}, oldState, action.comments);
+        case  RECEIVE_COMMENT:
+            return Object.assign({}, oldState, { [action.comment.id]: action.comment });
+        case REMOVE_COMMENT:
+            let nextState = Object.assign({}, oldState);
+            delete nexState[action.commentId];
+            return nextState;
+        default:
+            return oldState;
+    };
+};
+
+export default commentsReducer;
