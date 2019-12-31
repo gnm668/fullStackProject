04191a4bf2475767441d2f7975252b1e9a6df502
Al <nguyenvtalexander@gmail.com>
2019-12-30 17:34:13 -0800

    star comment action

---
+++ frontend/actions/comment_actions.js
@@ -0,0 +1,7 @@
+import * as APIUtil from '../util/comment_util';
+
+export const RECEIVE_ALL_COMMENTS = 'RECEIVE_ALL_COMMENTS';
+export const RECEIVE_COMMENT = 'RECEIVE_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+
+
