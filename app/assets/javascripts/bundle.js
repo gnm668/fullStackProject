5c0d8dea34280ee6f85c3254b5a7608125ac341b
Al <nguyenvtalexander@gmail.com>
2020-01-02 13:10:12 -0800

    home button

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -688,10 +688,16 @@ function (_React$Component) {
         id: 'ham',
         className: hamburgerClass.join(' ')
       }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("svg", {
-        src: "/Users/al/Desktop/FSP/Tube/app/assets/images/home-button.svg",
         className: "home",
-        onClick: this.homeRedirect
-      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("svg", {
+        onClick: this.homeRedirect,
+        xmlns: "http://www.w3.org/2000/svg",
+        width: "24",
+        height: "24",
+        viewBox: "0 0 24 24"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("path", {
+        fill: "#FFFFFF",
+        d: "M21 13v10h-6v-6h-6v6h-6v-10h-3l12-12 12 12h-3zm-1-5.907v-5.093h-3v2.093l3 3z"
+      })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("svg", {
         src: "/Users/al/Desktop/FSP/Tube/app/assets/images/you-tube-2017-icon-seeklogo.com-4.svg",
         className: "logo",
         onClick: this.homeRedirect
