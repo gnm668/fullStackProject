147e4508f44f4d61afc6fd3bac176933bfe2bed9
Al <nguyenvtalexander@gmail.com>
2020-01-02 17:46:05 -0800

    search feed and search page

---
+++ frontend/components/feeds/search_feed.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import VideoFeedItem from '../video/video_feed_item';
+
+class SearchFeed extends React.Component {
+    constructor(props) {
+        super(props);
+    };
+
+    render() {
+        return(
+            <div className='search-feed'>
+
+            </div>
+        );
+    };
+};
+
+export default SearchFeed;
---
+++ frontend/components/main/search.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import SearchFeedContainer from '../feeds/search_feed_container';
+import SideBar from '../bars/side_bar';
+import NavBarContainer from '../bars/nav_bar_container';
+
+class Search extends React.Component {
+    constructor(props) {
+        super(props);
+    };
+
+    render() {
+        return(
+            <div className='search'>
+
+            </div>
+        );
+    };
+};
